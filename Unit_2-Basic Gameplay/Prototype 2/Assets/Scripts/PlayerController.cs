2f45507c0e8e4b2d3474623826ac4aca0d57f0b7
46013176 <mathew.maggio@go.sfcollege.edu>
2024-09-11 11:21:53 -0400

    Lesson 2.2 Complete

--- Unit_2-Basic Gameplay/Prototype 2/Assets/Scripts/PlayerController.cs
+++ Unit_2-Basic Gameplay/Prototype 2/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10;
+    public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +17,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            //Launch projectile
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+
         //Keep Player in bounds
         if(transform.position.x < -xRange)
         {
