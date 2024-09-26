f04a74d4251c522a0f69eb8c21933e7ef7edb60d
46013176 <mathew.maggio@go.sfcollege.edu>
2024-09-26 08:51:23 -0400

    Lesson 4.4 Done

--- Unit_4-Gameplay Mechanics/Prototype 4/Assets/Scripts/Enemy.cs
+++ Unit_4-Gameplay Mechanics/Prototype 4/Assets/Scripts/Enemy.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,7 @@ public class Enemy : MonoBehaviour
     public float speed;
     private Rigidbody enemyRb;
     private GameObject player;
+
 
     // Start is called before the first frame update
     void Start()
@@ -18,8 +20,16 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         Vector3 lookDirection = ((player.transform.position - transform.position).normalized);
 
         enemyRb.AddForce(lookDirection * speed);
+
+        if(transform.position.y < -10)
+        {
+            Destroy(gameObject);
+        }
+
+        
     }
 }
